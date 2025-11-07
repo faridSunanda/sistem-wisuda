4c8495f57b8e9fd695029ba48ed207305f578d95
ArvianRanditya <arvianrandityaa@gmail.com>
2025-11-07 11:12:21 +0700

    menambah padding

--- resources/views/mahasiswa/data-diri/index.blade.php
+++ resources/views/mahasiswa/data-diri/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="max-w-4xl mx-auto"> 
+<div class="max-w-4xl mx-auto pb-10"> 
 
     @if (session('success'))
         <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
